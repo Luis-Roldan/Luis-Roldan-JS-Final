6032090bd38d710600f57f83c1906c9a11233844
Luis Roldan <roldanluis022@gmail.com>
2022-11-07 13:39:22 -0500

    reentrega final

--- index.js
+++ index.js
@@ -1,38 +1,38 @@
-let nombreIngresado = prompt("Ingresar Nombre");
-let apellidoIngresado = prompt("Ingresar Apellido")
+// let nombreIngresado = prompt("Ingresar Nombre");
+// let apellidoIngresado = prompt("Ingresar Apellido")
 
 
 
-function solicitarNombre(nombreIngresado , apellidoIngresado){
-    if((nombreIngresado == "") || (apellidoIngresado == "")){
+// function solicitarNombre(nombreIngresado , apellidoIngresado){
+//     if((nombreIngresado == "") || (apellidoIngresado == "")){
 
-        Swal.fire({
-            icon: "error",
-            tittle: "error al procesar",
-            text: "No ingresaste la informacion completa"
-        })
-    }
-    else{
+//         Swal.fire({
+//             icon: "error",
+//             tittle: "error al procesar",
+//             text: "No ingresaste la informacion completa"
+//         })
+//     }
+//     else{
         
-        Swal.fire({
-            icon: "success",
-            tittle: "Bienvenido",
-            text: "Bienvenido a Tienda online estimado" + " " + nombreIngresado + " " + apellidoIngresado + "," + " disfruta de tus compras con nosotros",
-            width: 400,
-            padding: '3em',
-            color: '#716add',
-            background: '#fff url(/images/trees.png)',
-            backdrop: `
-              rgba(0,0,123,0.4)
-              url("/img/nyan-cat.gif")
-              left top
-              no-repeat
-            `
-          })
-    }
-}
+//         Swal.fire({
+//             icon: "success",
+//             tittle: "Bienvenido",
+//             text: "Bienvenido a Tienda online estimado" + " " + nombreIngresado + " " + apellidoIngresado + "," + " disfruta de tus compras con nosotros",
+//             width: 400,
+//             padding: '3em',
+//             color: '#716add',
+//             background: '#fff url(/images/trees.png)',
+//             backdrop: `
+//               rgba(0,0,123,0.4)
+//               url("/img/nyan-cat.gif")
+//               left top
+//               no-repeat
+//             `
+//           })
+//     }
+// }
 
-solicitarNombre(nombreIngresado , apellidoIngresado)
+// solicitarNombre(nombreIngresado , apellidoIngresado)
 
 // const products = [ { name: "lampara", price: "69.00" },  { name: "Mesa Gamer", price: "699.00" }]
 
@@ -44,45 +44,41 @@ solicitarNombre(nombreIngresado , apellidoIngresado)
 
 // alert("Costo final de los productos seleccionados lampara y mesa Gamer"+ " " + montoFinal + "," + " " + "Gracias por su preferencia")
 
-function calcular(){
-    let continuar = true
+// function calcular(){
+//     let continuar = true
 
-    while(continuar==true){    
+//     while(continuar==true){    
         
         
-        let ingresarNumero = parseInt(prompt("Ingresar Numero (0 para salir)"))
+//         let ingresarNumero = parseInt(document.getElementById("Ingresar Numero (0 para salir)"))
         
-        let resultado = ingresarNumero + ingresarNumero * 0.18;
+//         let resultado = ingresarNumero + ingresarNumero * 0.18;
 
-        alert("El costo con impuestos es" + " " + resultado + " " + "soles");
+//         alert("El costo con impuestos es" + " " + resultado + " " + "soles");
 
 
-        if(ingresarNumero==0){
-        	continuar=false;
-	} else{
-        	sessionStorage.setItem("producto", resultado)
+//         if(ingresarNumero==0){
+//         	continuar=false;
+// 	} else{
+//         	sessionStorage.setItem("producto", resultado)
 
-        	let productoGuardado = sessionStorage.getItem("producto")
+//         	let productoGuardado = sessionStorage.getItem("producto")
 
-        	console.log(productoGuardado)
-        }
-    }
+//         	console.log(productoGuardado)
+//         }
+//     }
 
     
-}
+// }
 
 
-const botonSwal = document.getElementById("btnMostrarSwal")
-botonSwal.onclick = mostrarSwal
+// const botonSwal = document.getElementById("btnMostrarSwal")
+// botonSwal.onclick = mostrarSwal
 
 
 function consultarProductosServer() {
-    fetch("https://634753a3db76843976a9f41e.mockapi.io/")
+    fetch("https://634753a3db76843976a9f41e.mockapi.io/Ecommerce")
     .then((response) => response.json())
     .then((data) => console.log(data))
     .catch((error) => console.log(error));
 }
-
-
-
- 
